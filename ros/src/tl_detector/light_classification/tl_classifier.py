70c6322137be618586690315ee9b4cfc3ece18ac
ernestocanibano <ernestoecr@gmail.com>
2019-08-27 15:49:59 +0200

    provisional submission

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,19 +1,112 @@
 from styx_msgs.msg import TrafficLight
+import tensorflow as tf
+import numpy as np
+import os
+import cv2
+
+import visualization_utils as vis_util
+
+import rospy #to remove
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+IMAGE_WIDTH = 300
+IMAGE_HEIGHT = 300
+MIN_SCORE = 0.5
 
 class TLClassifier(object):
-    def __init__(self):
-        #TODO load classifier
-        pass
+	def __init__(self, is_site=False):
+		#TODO load classifier
+		# Just for debugging
+		#self.bridge = CvBridge()
+		#self.image_publisher = rospy.Publisher('image_predicted', Image, queue_size=1)
+		#self.category_index = {1: {'id': 1, 'name': u'red'}, 2: {'id': 2, 'name': u'yellow'}, 3: {'id': 3, 'name': u'green'}, 4: {'id': 4, 'name': u'off'}}
 
-    def get_classification(self, image):
-        """Determines the color of the traffic light in the image
+		self.classes = {1: TrafficLight.RED,
+						2: TrafficLight.YELLOW,
+						3: TrafficLight.GREEN,
+						4: TrafficLight.UNKNOWN}
+		
+		if is_site:
+			self.path_to_ckpt = os.path.dirname(os.path.realpath(__file__))+'/models/ssd_inception_v2_real_graph.pb'
+		else:
+			self.path_to_ckpt = os.path.dirname(os.path.realpath(__file__))+'/models/ssd_inception_v2_sim_graph.pb'
+		
+		# optimization options
+		config = tf.ConfigProto()
+		config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
+		config.gpu_options.allow_growth = True
+		config.gpu_options.per_process_gpu_memory_fraction = 0.6
+		
+		# load the graph
+		self.detection_graph = tf.Graph()
+		with self.detection_graph.as_default():
+			od_graph_def = tf.GraphDef()
+			with tf.gfile.GFile(self.path_to_ckpt, 'rb') as fid:
+				serialized_graph = fid.read()
+				od_graph_def.ParseFromString(serialized_graph)
+				tf.import_graph_def(od_graph_def, name='')
+		
+		# init detection
+		self.session = tf.Session(graph=self.detection_graph, config=config)
+		self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+		self.boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+		self.scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+		self.classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+		self.num_detections = self.detection_graph.get_tensor_by_name('num_detections:0')
+    
+	def load_image_into_numpy_array(self, image):
+		(im_width, im_height) = image.size
+		return np.array(image.getdata()).reshape((im_height, im_width, 3)).astype(np.uint8)    
+		
+       
+	def get_classification(self, image):
+		"""Determines the color of the traffic light in the image
+		
+		Args:
+			image (cv::Mat): image containing the traffic light
+		
+		Returns:
+			int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+		
+		"""
+		image = cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT))
+		image_np = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		image_np_expanded = np.expand_dims(image_np, axis=0)
+		
+		# Actual detection.
+		(boxes, scores, classes, num_detections) = self.session.run( [self.boxes, self.scores, self.classes, self.num_detections], feed_dict={self.image_tensor: image_np_expanded})
+		
+		scores = np.squeeze(scores)
+		classes = np.squeeze(classes)
+		boxes = np.squeeze(boxes)
+		
+		# Visualization of the results of a detection. Just for debugging
+		#vis_util.visualize_boxes_and_labels_on_image_array( image_np, boxes, classes.astype(np.int32), scores, self.category_index, use_normalized_coordinates=True, line_thickness=4)
+		#image = self.prediction(image_np)
+		#image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+		#self.image_publisher.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
+		
+		num_red = 0
+		num_yellow = 0
+		num_green = 0
+		for i, box in enumerate(boxes):
+			if scores[i] > MIN_SCORE:
+				if classes[i] == 1:
+					num_red = num_red + 1
+				elif classes[i] == 3:
+					num_green = num_green + 1
+				elif classes[i] == 2:
+					num_yellow = num_yellow + 1
+		
+		if(num_red >= num_green and num_red >= num_yellow and num_red > 0):
+			light_class = TrafficLight.RED
+		elif(num_green > num_red and num_green > num_yellow and num_green > 0):
+			light_class = TrafficLight.GREEN	
+		elif(num_yellow > num_red and num_yellow >= num_green and num_yellow > 0):
+			light_class = TrafficLight.YELLOW
+		else:
+			light_class = TrafficLight.UNKNOWN
 
-        Args:
-            image (cv::Mat): image containing the traffic light
-
-        Returns:
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
-        """
-        #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+        
+		return light_class
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+from std_msgs.msg import Int32
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
 from scipy.spatial import KDTree
@@ -24,34 +25,37 @@ TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
 LOOKAHEAD_WPS = 100 # Number of waypoints we will publish. You can change this number
-
+MAX_DECEL = 0.5
 
 class WaypointUpdater(object):
     def __init__(self):
-        rospy.init_node('waypoint_updater')
+		rospy.init_node('waypoint_updater')
+		
+		rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
+		rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
+		
+		# TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
+		rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
+		
+		self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
+		
+		# Obtain the speed limit from the server
+		self.speed_limit = rospy.get_param('waypoint_loader/velocity', 40) 
+				
+		# TODO: Add other member variables you need below
+		self.pose = None
+		self.stopline_wp_idx = -1
+		self.waypoints_2d = None
+		self.waypoint_tree = None
+		self.base_lane = None
+		
+		rate = rospy.Rate(50)
+		while not rospy.is_shutdown():
+			if self.pose and self.base_lane:
+				self.publish_waypoints()
+			rate.sleep()
 
-        rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
-        rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
-        # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
-
-        self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
-
-        # TODO: Add other member variables you need below
-        self.pose = None
-        self.base_waypoints = None
-        self.waypoints_2d = None
-        self.waypoint_tree = None
-
-        rate = rospy.Rate(50)
-        while not rospy.is_shutdown():
-            if self.pose and self.base_waypoints:
-                closest_waypoint_idx = self.get_closest_waypoint_id()
-                self.publish_waypoints(closest_waypoint_idx)
-            rate.sleep()
-
-    def get_closest_waypoint_id(self):
+    def get_closest_waypoint_idx(self):
         x = self.pose.pose.position.x
         y = self.pose.pose.position.y
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
@@ -71,12 +75,46 @@ class WaypointUpdater(object):
             closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
         return closest_idx
 
-    def publish_waypoints(self, closest_idx):
-        # create a lane message and send the next LOOKAHEAD_WPS points
-        lane = Lane()
-        lane.header = self.base_waypoints.header
-        lane.waypoints = self.base_waypoints.waypoints[closest_idx:(closest_idx + LOOKAHEAD_WPS)]
-        self.final_waypoints_pub.publish(lane)
+    def publish_waypoints(self):
+        final_lane = self.generate_lane()
+        self.final_waypoints_pub.publish(final_lane)
+        
+    def generate_lane(self):
+		lane = Lane()
+        
+		closest_idx = self.get_closest_waypoint_idx()
+		farthest_idx = closest_idx + LOOKAHEAD_WPS
+		base_waypoints = self.base_lane.waypoints[closest_idx:farthest_idx]
+        
+		## control de speed limit
+		for i, wp in enumerate(base_waypoints):
+			base_waypoints[i].twist.twist.linear.x = min(self.speed_limit*0.27778, base_waypoints[i].twist.twist.linear.x)
+            
+		if self.stopline_wp_idx == -1 or (self.stopline_wp_idx >= farthest_idx):
+			lane.waypoints = base_waypoints
+		else:
+			lane.waypoints = self.decelerate_waypoints(base_waypoints, closest_idx)
+            
+		return lane
+ 
+       
+    def decelerate_waypoints(self, waypoints, closest_idx):
+		temp = []
+		
+		for i, wp in enumerate(waypoints):
+			p = Waypoint()
+			p.pose = wp.pose
+			
+			stop_idx = max(self.stopline_wp_idx - closest_idx - 2, 0) # stop 2 waypoints before the stop line
+			dist = self.distance(waypoints, i, stop_idx)
+			vel = math.sqrt(2 * MAX_DECEL * dist)
+			if vel < 1.0:
+				vel = 0.0
+				
+			p.twist.twist.linear.x = min(vel, self.get_waypoint_velocity(wp))
+			temp.append(p)
+			
+		return temp
 
     def pose_cb(self, msg):
         # TODO: Implement
@@ -84,14 +122,14 @@ class WaypointUpdater(object):
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        self.base_waypoints = waypoints
+        self.base_lane = waypoints
         if not self.waypoints_2d:
             self.waypoints_2d = [[waypoint.pose.pose.position.x, waypoint.pose.pose.position.y] for waypoint in waypoints.waypoints]
             self.waypoint_tree = KDTree(self.waypoints_2d)
 
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+        self.stopline_wp_idx = msg.data
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
